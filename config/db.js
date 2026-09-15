fc98fbf240059af6132e40b521672255f7ccfd8a
Devjoyti Ball <devjoytiball.24@gmail.com>
2026-09-15 16:34:12 +0530

    Fix Raolway MySQL connection

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const db = mysql.createPool({
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
+    port:process.env,MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
