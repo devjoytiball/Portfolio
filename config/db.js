c0c48f79617c1203b3bdd78f98b57566c05693c4
Devjoyti Ball <devjoytiball.24@gmail.com>
2026-09-15 19:18:24 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const db = mysql.createPool({
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port:process.env,MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
