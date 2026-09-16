3b73fe79f94292ab9b716890aa93decce3d3e064
Devjoyti Ball <devjoytiball.24@gmail.com>
2026-09-16 16:32:10 +0530

    initial commit

--- app.js
+++ app.js
@@ -1563,7 +1563,7 @@ app.use((err, req, res, next) => {
 
 });
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT,"0.0.0.0",()=>{
     console.log(`server running on port ${PORT}`);
 });
